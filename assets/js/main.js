9fb591e7660fbb8ca440a57b9dd9d1d636143101
Doan Thi Thuy Linh <doantthuylinh98@gmail.com>
2022-10-10 20:09:25 +0700

    Update responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,15 +104,17 @@ function scrollActive() {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 50;
     const sectionId = current.getAttribute('id');
-
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector('.nav__menu a[href*=' + sectionId + ']')
-        .classList.add('active-link');
-    } else {
-      document
-        .querySelector('.nav__menu a[href*=' + sectionId + ']')
-        .classList.remove('active-link');
+    const link = $('.nav__menu a[href*=' + sectionId + ']');
+    if (link) {
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        document
+          .querySelector('.nav__menu a[href*=' + sectionId + ']')
+          .classList.add('active-link');
+      } else {
+        document
+          .querySelector('.nav__menu a[href*=' + sectionId + ']')
+          .classList.remove('active-link');
+      }
     }
   });
 }
